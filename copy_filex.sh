fcf9a7b88df58ede2f6734f88d3acc59edc57d37
Artemy Gladkov <gladkov.art76@gmail.com>
2024-09-20 11:22:08 +0300

    initial

---
+++ copy_filex.sh
@@ -0,0 +1,15 @@
+
+cp  ".\lexical analysis\lexical analysis.pdf" ".\1. Лексический анализ.pdf"
+echo ".\1. Лексический анализ.pdf" copied
+
+cp  ".\syntax analysis 1\syntax analysis 1.pdf" ".\2. Синтаксический анализ.pdf"
+echo ".\2. Синтаксический анализ.pdf" copied
+
+cp  ".\context conditions\context conditions.pdf" ".\3. Семантический анализ (проверка контекстных условий).pdf"
+echo ".\3. Семантический анализ (проверка контекстных условий).pdf" copied
+
+cp  ".\internal generation\internal generation.pdf" ".\4. Генерация внутренних представлений (ПОЛИЗ).pdf"
+echo ".\4. Генерация внутренних представлений (ПОЛИЗ).pdf" copied
+
+cp  ".\interpretation\interpretation.pdf" ".\5. Исполнение.pdf"
+echo ".\5. Исполнение.pdf" copied
